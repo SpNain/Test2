12e33cbf89e00da6cb7f4811916c4bf624f1d1e5
HardeepNain <hnspnain@gmail.com>
2025-02-09 13:43:27 +0530

    etw1

--- ExpenseTracker-fullstack-master/controllers/mainpage.js
+++
@@ -1,8 +0,0 @@
-
-exports.gethomePage = (request, response, next) => {
-    response.sendFile('home.html', { root: 'views' });
-}
-exports.geterrorPage = (request,response,next) =>{
-    response.sendFile('notFound.html',{root:'views'});
-}
-
--- ExpenseTracker-fullstack-master/util/database.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('expense-tracker','root','T#9758@qlph',{
-    dialect:'mysql',
-    host:'localhost',
-    logging:false
-});
-module.exports = sequelize;
