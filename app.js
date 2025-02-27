6d7fb7992a9eed92209a3b7460fe12c632dabad8
HardeepNain <hnspnain@gmail.com>
2025-02-27 21:59:40 +0530

    task 3 completed

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const app = express();
 const bodyParser = require("body-parser");
 
 const cors = require("cors");
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 const dotenv = require("dotenv");
 dotenv.config();
