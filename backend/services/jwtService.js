95d6898b4b191d2fb9b9bf0b33c37b0c70ecd934
HardeepNain <hnspnain@gmail.com>
2025-03-13 13:17:49 +0530

    kindred hearts : 3 day test project completed

--- backend/services/jwtService.js
+++
@@ -1,7 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-exports.generateAccessToken = (id, email, role) => {
-  return jwt.sign({ id: id, email: email, role: role }, process.env.TOKEN_SECRET_KEY, {
-    expiresIn: "4h",
-  });
-};
